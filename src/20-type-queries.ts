e2c84d02596ea5bb73c8f02e841c2056393b59a3
SamuilG <samuil4@gmail.com>
2018-06-19 15:33:07 +0300

    added type queries example

--- src/20-type-queries.ts
+++ src/20-type-queries.ts
@@ -2,6 +2,22 @@
 const developer = {
   name: 'Sam',
   xp: 10,
+  test: {
+    num1: 1,
+    str1: '1',
+  },
+};
+
+type DevType = typeof developer;
+
+const newDeveloper: DevType = {
+  name: 'Naka',
+  xp: 10,
+  test: {
+    num1: 1,
+    str1: '1',
+  },
 };
 
 // Use typeof operator to create a new type of developer
+// Keysof
